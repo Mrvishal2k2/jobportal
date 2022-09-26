e141c9c8ef1603f6c9c9ca3d460da67e442e9ab7
Vishal R <67860986+Mrvishal2k2@users.noreply.github.com>
2022-09-26 12:36:15 +0530

    how i missed seeing this

--- admin/header.php
+++ admin/header.php
@@ -5,8 +5,6 @@
   mrvishal2k2@pm.me
  -->
 <?php
-// Initialize the session
-session_start();
  
 // Check if the user is logged in, if not then redirect him to login page
 if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
